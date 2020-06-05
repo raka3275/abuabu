c2c80790e441437e85e9e9f718b512283561a912
abuusamah <49653679+abuusamah@users.noreply.github.com>
2020-06-05 12:05:27 +0700

    Update bakpia.php

--- bakpia.php
+++ bakpia.php
@@ -93,7 +93,7 @@ echo color("white","                  Format Kode 62*** \n");
         sleep(3);
         }
         sleep(3);
-        $boba09 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"EBADAHMAKANA"}');
+        $boba09 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"COBAGOFOOD260520"}');
         $messageboba09 = fetch_value($boba09,'"message":"','"');
         echo "\n".color("white"," Message: ".$messageboba09);
         sleep(1);
