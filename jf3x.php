a0bdd1e95b24ab8ca6de384f888a75ae1a110c3a
abuusamah <49653679+abuusamah@users.noreply.github.com>
2020-06-23 21:07:56 +0700

    Update jf3x.php

--- jf3x.php
+++ jf3x.php
@@ -75,7 +75,7 @@ function veriflogin($otp, $token)
 	}
 function claim($token)
 	{
-	$data = '{"promo_code":"PESANGOFOOD0906"}';
+	$data = '{"promo_code":"EATLAH"}';
 	$claim = request("/go-promotions/v1/promotions/enrollments", $token, $data);
 	if ($claim['success'] == 1)
 		{
