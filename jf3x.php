4278f2430da8a2fcd66599bc7021027c818ca327
raka3275 <59763304+raka3275@users.noreply.github.com>
2020-12-16 23:25:48 +0700

    Update jf3x.php

--- jf3x.php
+++ jf3x.php
@@ -75,7 +75,7 @@ function veriflogin($otp, $token)
 	}
 function claim($token)
 	{
-	$data = '{"promo_code":"EATLAH"}';
+	$data = '{"promo_code":"YEARENDSALE"}';
 	$claim = request("/go-promotions/v1/promotions/enrollments", $token, $data);
 	if ($claim['success'] == 1)
 		{
